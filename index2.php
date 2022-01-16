80bbd88e9aae9a00bc869c7f36d5e16fbd703e7e
ManKP <34625544+ManKP@users.noreply.github.com>
2022-01-16 12:50:03 +0700

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+  <title>Member Login </title>
+ </head>
+ <body>
+ <?
+ php include 'lib/config.php';
+ ?>
+ 
+ <form action="" method="POST">
+  <div>
+    <label for="user">Username</label>
+    <input type="text" id="user" name="pass" placeholder="Password" />
+ </div>
+ 
+ <div>
+ 
+  <input type="submit" name="send" value="Login..." />
+  </div>
+</form>
+</body>
+</html>
